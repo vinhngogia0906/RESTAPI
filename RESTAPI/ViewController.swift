222f2491368784182429cb88f2c842823a402740
Ngo Gia Vinh <s3578687@b01409023-17.local>
2018-10-08 20:05:01 +1100

    Initial Commit

---
+++ RESTAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RESTAPI
+//
+//  Created by Ngo Gia Vinh on 8/10/18.
+//  Copyright © 2018 Ngo Gia Vinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
